78afb379a707b980805256f2fa1eaad04a2e2119
João Gabriel FA <112104535+JoaoGabrielFA@users.noreply.github.com>
2023-08-31 14:06:28 -0300

    Add files via upload

---
+++ ScreenSound/3.0/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,29 @@
+﻿using OpenAI_API;
+using ScreenSound.Models;
+
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        /*var client = new OpenAIAPI("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");
+        var chat = client.Chat.CreateConversation();
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal.");
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;*/
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("\nDigite uma tecla para votar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
